82e680841e94c82c3118731b1bd558ea24fada34
Emil Vaklinov <emil_vaklinov@yahoo.com>
2021-06-15 16:41:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MailTo from "./mail-to";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MailTo />
+    </div>
+  );
+}
